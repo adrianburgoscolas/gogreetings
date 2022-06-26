15b50719cd78a39a25449bc225311bb1afea123b
Adrian Burgos <adrian.burgos.colas@gmail.com>
2022-06-25 22:48:11 -0400

    space separate name

--- greeting.go
+++ greeting.go
@@ -1,9 +1,9 @@
-package gogreetings
+package greeting
 
 func Hello(s string) string {
-	return "Hello" + s
+	return "Hello " + s
 }
 
 func Hi(s string) string {
-	return "Hi" + s
+	return "Hi " + s
 }
